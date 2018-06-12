4efbdc14e82d81686916ce23849a1c9d407e7857
Golfist <stexmachina@gmail.com>
2018-06-12 16:48:20 +0300

    Update Muscle-car.py

--- Muscle-car.py
+++ Muscle-car.py
@@ -6,21 +6,21 @@ import re
 cars = urlopen("https://americanmusclecar.ru")
 cars = bs(cars, "html.parser")
 cars = str(cars)
-catal = re.compile('g/\D+\/"') #Регулярное выражение для поиска авто
-catalog = catal.findall(cars)  #Поиск авто по выражению
+catal = re.compile('g/\D+\/"') #Expression for searching cars references
+catalog = catal.findall(cars)  #Function of searching
 count = 0
-while count != len(catalog):
-    ref = catalog[count]
-    car = urlopen("https://americanmusclecar.ru/catalog" + ref[1 : -1])
-    car = bs(car, "html.parser")
-    car = car.find_all('p')
-    f = open(ref[2 : -2] + '.txt', 'w')
-    for mcar in car:
+while count != len(catalog): #Parse the catalog of cars
+    ref = catalog[count] # choose ref in catalog
+    car = urlopen("https://americanmusclecar.ru/catalog" + ref[1 : -1]) #firstly cleaning up ref for opening like a object
+    car = bs(car, "html.parser") #parse 
+    car = car.find_all('p') #find all text
+    f = open(ref[2 : -2] + '.txt', 'w') # create file of car
+    for mcar in car:  #cleaning info and then whiting to file
         car = mcar.get_text()
         f.write(car)
         f.write('\t')
-    f.close
-    count += 1
+    f.close #closed file
+    count += 1 #increase count
     
 
 
